6089f05ccd300e57d5a151b6f9ffa3ccab8eb724
Baron Zhang <baron.zhang@avepoint.com>
2018-06-27 11:46:24 +0800

    Add ps tool project

---
+++ ps-tools/ps-tools.ps1
@@ -0,0 +1,6 @@
+﻿#
+# Script.ps1
+#
+Write-Host "This is a ps tool project which help the cloud governance customers"
+Write-Host "Tool list:"
+Write-Host "#1 Upgrade GAO App and Change Card to Panel"
